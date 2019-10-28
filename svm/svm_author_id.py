0a7103587b0309f07d4b5641a7480cc0936b8887
guillain bisimwa <guillainbisimwa@hotmail.fr>
2019-10-28 14:10:08 -0400

    SVM training and prediction

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -24,7 +24,28 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 #########################################################
 ### your code goes here ###
+from sklearn.svm import SVC
+clf = SVC(C=1.0, kernel='rbf', gamma='auto')
+t = time()
 
+#features_train = features_train[:len(features_train)/100]
+#labels_train = labels_train[:len(labels_train)/100]
+
+clf.fit(features_train, labels_train)
+print "Trainning time: ", round(time()-t,3), "s"
+tt = time()
+pred = clf.predict(features_test)
+print "Predicting time: ", round(time()-tt,3), "s"
+accuracy = clf.score(features_test, labels_test)
+print accuracy
+print "Predict the element [10]: ", pred[10]
+print "Predict the element [25]: ", pred[26]
+print "Predict the element [50]: ", pred[50]
 #########################################################
-
+print pred.size
+chris = 0
+for row in pred:
+    if row == 0:
+        chris += 1 
+print chris
 
