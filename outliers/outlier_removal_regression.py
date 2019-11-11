e98b9537055833319f8856cd4e77f09a450592fd
guillain bisimwa <guillainbisimwa@hotmail.fr>
2019-11-11 14:44:51 -0500

    Score of regression with outlier

--- outliers/outlier_removal_regression.py
+++ outliers/outlier_removal_regression.py
@@ -26,10 +26,13 @@ ages_train, ages_test, net_worths_train, net_worths_test = train_test_split(ages
 ### fill in a regression here!  Name the regression object reg so that
 ### the plotting code below works, and you can see what your regression looks like
 
+from sklearn import linear_model
+reg = linear_model.LinearRegression()
+reg.fit(ages_test, net_worths_test)
 
-
-
-
+print "SCORE: ", reg.score(ages_test, net_worths_test)
+print "Intersept: ", reg.intercept_
+print "Slope: ", reg.coef_
 
 
 
